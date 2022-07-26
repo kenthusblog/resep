43144266d1d8f11a05ab0806a78da92b515ee74b
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-07-26 08:32:15 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="mt-5">
       <div className="py-4 px-5 bg-dark-800 text-white font-semibold text-center ">
-        <div className="capitalize">Ayo Masak &copy; 2022</div>
+        <div className="capitalize">Pawonan &copy; 2022 <br/>Jatim     &#10084;&#65039; Indonesia </div>
       </div>
     </footer>
   );
